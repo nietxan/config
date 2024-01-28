a6180e1dbbdd6cf137b56a66017bbf7b8548dc23
Niyetkhan <niethan.2004@gmail.com>
2024-01-28 19:21:08 +0600

    gitignore fix

---
+++ after/plugin/gruvbox.lua
@@ -0,0 +1,6 @@
+require('gruvbox').setup({
+	terminal_colors = true,
+	transparent_mode = true,
+	contrast = "hard"
+})
+vim.cmd('colorscheme gruvbox')
---
+++ after/plugin/psql.lua
@@ -0,0 +1,8 @@
+require('psql').setup({
+	database_name = 'nietxan',
+	execute_line        = '<leader>e',
+	execute_selection   = '<leader>e',
+
+	close_latest_result = '<leader>w',
+	close_all_results   = '<leader>W',
+})

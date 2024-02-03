77ed0154da8e311d3b96d213493acbe86a42c9f2
Niyetkhan <niethan.2004@gmail.com>
2024-02-03 14:55:09 +0600

    last

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mv ../config ~/.config/nvim
+mkdir ~/.config/nvim
+
+mv * .[^.]* ~/.config/nvim
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 wait $BACK_PID
-
-nvim +so +PackerSync 2> /dev/null

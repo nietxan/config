e6f8c05af287f98ec1284d79eeb3cacffdc5955a
Niyetkhan <niethan.2004@gmail.com>
2024-02-03 12:13:09 +0600

    better repo mv and Packer

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-mv * ~/.config/nvim/
+rm ~/.config/nvim
+mv ../config ~/.config/nvim
+
+// Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+cd ~/.config/nvim
+
+nvim --headless +so +PackerInstall +PackerSync +qall

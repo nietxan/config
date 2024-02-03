a205219e6299072c9dd7f722683612470801e73d
Niyetkhan <niethan.2004@gmail.com>
2024-02-03 12:33:20 +0600

    third try

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-rm -f ~/.config/nvim
 mv ../config ~/.config/nvim
 
-// Packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-cd ~/.config/nvim
+wait $BACK_PID
 
 nvim ~/.config/nvim/lua/plugins.lua +so +PackerSync +qall
-
-nvim
